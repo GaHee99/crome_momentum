fe6bc7ae3d8817487d0e09787b154f35317dd19b
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2021-11-22 18:22:16 +0900

    username가져오기

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+const HIDDEN_CLASSNAME = 'hidden'
 
 function onLoginSubmit(event) {
-  //이렇게 공간을 만들어주면 js에서 event를 채워준다.
-  event.preventDefault() //브라우저가 기본 동작을 실행하지 못하게 막아주는 것
-  console.log(loginInput.value)
+  event.preventDefault()
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const username = loginInput.value
+  greeting.innerText = `Hello ${username}`
+  //=> greeting.innerText="Hello" + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
+
 loginForm.addEventListener('submit', onLoginSubmit)
-//js가 어떤 정보를 담은채로 onLoginSubmit이라는 function을 호출한다.
