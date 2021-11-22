a04c3010fec92d3a8bb60c245f793537d176cb66
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2021-11-22 16:12:20 +0900

    submit event의 새로고침 막기

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
-const loginButton = document.querySelector('#login-form button')
 
-function onLoginBtnClick() {
-  const username = loginInput.value
-  console.log('username')
+function onLoginSubmit(event) {
+  //이렇게 공간을 만들어주면 js에서 event를 채워준다.
+  event.preventDefault() //브라우저가 기본 동작을 실행하지 못하게 막아주는 것
+  console.log(loginInput.value)
 }
-
-loginButton.addEventListener('click', onLoginBtnClick)
+loginForm.addEventListener('submit', onLoginSubmit)
+//js가 어떤 정보를 담은채로 onLoginSubmit이라는 function을 호출한다.
