acd504b96cfdadae002a30ab4f648d4808d9c3f8
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2021-11-24 14:58:22 +0900

    clock마무리

--- js/clock.js
+++ js/clock.js
@@ -2,14 +2,11 @@ const clock = document.querySelector('h2#clock')
 
 function getClock() {
   const date = new Date()
-  const Hours = date.getHours()
-  const minutes = date.getMinutes()
-  const second = date.getSeconds()
+  const Hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const second = String(date.getSeconds()).padStart(2, '0')
   clock.innerText = `${Hours} : ${minutes} : ${second}`
 }
 getClock()
 //웹사이트가 로드되자마자 바로 실행시킬라고
 setInterval(getClock, 1000)
-//첫번째 인수는 내가 실행하고자 하는 function
-//두번째 인수는 호출되는 function간격을 몇초에 반복하는지
-//setInterval(sayHello, 5000)
